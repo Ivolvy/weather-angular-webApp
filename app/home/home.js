8fb1aa40f5bd34597a0760f2455dc8bd4cde2508
Ivolvy <michael.genty@free.fr>
2016-06-27 16:49:36 +0200

    Added city list and store all the data

--- app/home/home.js
+++ app/home/home.js
@@ -8,54 +8,12 @@ angular.module('myApp.home', ['ngRoute'])
             controller: 'HomeCtrl'
         });
     }])
-
-    .service('cityWeatherService', function($http,$q) {
-        function setWeatherFunctions(openWeatherData) {
-            this.data = openWeatherData;
-
-            this.getTemp = function() {
-                return this.data && this.data.main ? this.data.main.temp : null;
-            };
-            this.getWindSpeed = function() {
-                return this.data && this.data.wind ? this.data.wind.speed : null;
-            };
-            this.getWeatherDescription = function() {
-                return this.data && this.data.weather ? this.data.weather[0].description : null;
-            };
-            this.getMinTemp = function() {
-                return this.data && this.data.main ? this.data.main.temp_min : null;
-            };
-            this.getMaxTemp = function() {
-                return this.data && this.data.main ? this.data.main.temp_max : null;
-            };
-
-            this.getIconCode = function() {
-                return this.data && this.data.weather && this.data.weather[0] ? this.data.weather[0]['icon'] : null;
-            };
-            this.getIcon = function() {
-                return 'http://openweathermap.org/img/w/' + this.getIconCode() + '.png';
-            }
-        }
-
-        this.asyncGreet = function(city) {
-            var deferred = $q.defer();
-            var uri = 'http://api.openweathermap.org/data/2.5/weather' + "?q=" + city + "&units=metric&APPID=20a04174cafbc0d0b153d0edf7631f5c&callback=JSON_CALLBACK";
-            $http.jsonp(uri).
-                success(function(response, status) {
-                    deferred.resolve(new setWeatherFunctions(response));
-                }).error(function (error, status) {
-                    deferred.reject(error);
-                });
-
-            return deferred.promise;
-        }
-    })
     .directive('cityWeather', function(cityWeatherService, $compile) {
         return {
             restrict: 'AEC',
             controller: function(){
                 this.loadTemplate = function($scope, city){
-                    angular.element(document.getElementById('container')).append($compile("" +
+                    angular.element(document.getElementById('widget-container')).append($compile("" +
                         '<div id="'+city+'" class="city-name" ng-model="'+city+'">' +
                         '<h1 class="title">'+$scope.weather[city].city+'<img src="'+$scope.weather[city].icon+'"/></h1>' +
                         '<h3 class="tempW">'+$scope.weather[city].temp+'&deg;C</h3>' +
@@ -63,43 +21,25 @@ angular.module('myApp.home', ['ngRoute'])
                         '<h4 class="minAndMax">Min: '+$scope.weather[city].minTemp+'&deg;C, Max: '+$scope.weather[city].maxTemp+'&deg;C</h4>' +
                         '<h4 class="windSpeed">Wind speed: '+$scope.weather[city].windSpeed+'</h4>' +
                         '</div>')($scope));
-                }
+                };
+
             },
             link: function ($scope, element, attrs, cityWeatherController) {
 
                 //we retrieve all the city selected if they exists
                 if(Object.keys($scope.weather).length > 0){
+                    console.log($scope.weather);
                     for(var city in $scope.weather){
-                        cityWeatherController.loadTemplate($scope, city);
+                        //If we have already all the data
+                        if($scope.weather[city].dataFilled == true){
+                            cityWeatherController.loadTemplate($scope, city);
+                        }
                     }
                 }
-
-                //add a city
-                $scope.addCity = function() {
-                    if($scope.newCity != ""){
-                        $scope.city = $scope.newCity.replace(/ /g,""); //we delete all the whitespaces
-
-                        cityWeatherService.asyncGreet($scope.city).then(function(data) { //get all data for a city
-
-                            $scope.weather[$scope.city] = {
-                                city: $scope.city,
-                                icon: data.getIcon(),
-                                temp: data.getTemp(),
-                                weatherDescription: data.getWeatherDescription(),
-                                minTemp: data.getMinTemp(),
-                                maxTemp: data.getMaxTemp(),
-                                windSpeed: data.getWindSpeed(),
-                            };
-
-                            cityWeatherController.loadTemplate($scope, $scope.city);
-
-                        }, function() {alert('please insert a correct city')});
-                    }
-                };
             }
         };
 
     })
 
-    .controller('HomeCtrl', function($scope, cityWeatherService) {
+    .controller('HomeCtrl', function($scope) {
     });
--- app/settings/settings.js
+++ app/settings/settings.js
@@ -8,7 +8,111 @@ angular.module('myApp.settings', ['ngRoute'])
             controller: 'SettingsCtrl'
         });
     }])
+    .service('cityWeatherService', function($http,$q) {
+        //Here we have all the functions from openWeather api
+        function setWeatherFunctions(openWeatherData) {
+            this.data = openWeatherData;
 
-    .controller('SettingsCtrl', [function() {
+            this.getTemp = function() {
+                return this.data && this.data.main ? this.data.main.temp : null;
+            };
+            this.getWindSpeed = function() {
+                return this.data && this.data.wind ? this.data.wind.speed : null;
+            };
+            this.getWeatherDescription = function() {
+                return this.data && this.data.weather ? this.data.weather[0].description : null;
+            };
+            this.getMinTemp = function() {
+                return this.data && this.data.main ? this.data.main.temp_min : null;
+            };
+            this.getMaxTemp = function() {
+                return this.data && this.data.main ? this.data.main.temp_max : null;
+            };
 
-    }]);
+            this.getIconCode = function() {
+                return this.data && this.data.weather && this.data.weather[0] ? this.data.weather[0]['icon'] : null;
+            };
+            this.getIcon = function() {
+                return 'http://openweathermap.org/img/w/' + this.getIconCode() + '.png';
+            }
+        }
+
+        this.asyncGreet = function(city) {
+            var deferred = $q.defer();
+            var uri = 'http://api.openweathermap.org/data/2.5/weather' + "?q=" + city + "&units=metric&APPID=20a04174cafbc0d0b153d0edf7631f5c&callback=JSON_CALLBACK";
+            $http.jsonp(uri).
+                success(function(response, status) {
+                    deferred.resolve(new setWeatherFunctions(response));
+                }).error(function (error, status) {
+                    deferred.reject(error);
+                });
+
+            return deferred.promise;
+        }
+    })
+    .directive('weatherList', function(cityWeatherService, $compile) {
+        return {
+            restrict: 'AEC',
+            controller: function(){
+                //add a city
+                this.loadCity = function($scope, city) {
+                    if(city != ""){
+                        $scope.city = city.replace(/ /g,""); //we delete all the whitespaces
+
+                        cityWeatherService.asyncGreet($scope.city).then(function(data) { //get all data for a city
+
+                            $scope.weather[$scope.city] = {
+                                city: $scope.city,
+                                icon: data.getIcon(),
+                                temp: data.getTemp(),
+                                weatherDescription: data.getWeatherDescription(),
+                                minTemp: data.getMinTemp(),
+                                maxTemp: data.getMaxTemp(),
+                                windSpeed: data.getWindSpeed(),
+                                dataFilled: true,
+                            };
+
+                        }, function() {alert('please insert a correct city')});
+                    }
+                };
+                this.addCityInList = function($scope, city){
+                    angular.element(document.getElementById('weather-list')).append($compile('<li>' +
+                        '<span>'+city+'</span>' +
+                        '<div class="delete-city">delete</div>' +
+                        '</li>')($scope));
+                };
+            },
+            link: function ($scope, element, attrs, weatherListController) {
+
+                //we retrieve all the city selected if they exists
+                if(Object.keys($scope.weather).length > 0){
+                    for(var city in $scope.weather){
+                        //If we have already all the data
+                        if($scope.weather[city].dataFilled == true){
+                            weatherListController.addCityInList($scope, city);
+                        }
+                    }
+                }
+
+                //When we validate the form
+                $scope.addCity = function(){
+                    console.log('coucou');
+                    if($scope.newCity != ""){
+
+                        //Get the information for a city
+                        weatherListController.loadCity($scope, $scope.newCity);
+
+                        //Add th city in list
+                        weatherListController.addCityInList($scope, $scope.newCity);
+
+                        //Set input to empty
+                        angular.element(document.getElementById('new-city')).val('');
+                    }
+                }
+            }
+        };
+
+    })
+    .controller('SettingsCtrl', function($scope) {
+
+    });
